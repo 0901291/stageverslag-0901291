985e6fad4498adffa06ca0b69a50a9b4a2514efe
Ian Wensink <ianwensink@gmail.com>
2016-08-23 15:07:41 +0200

    Change app port

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(configureServer());
 
 app.set('view engine', 'pug');
 app.set('views', `${__dirname}/views`);
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
 
 app.get('*', (req, res) => {
   match({ routes, location: req.url }, (err, redirect, props) => {
