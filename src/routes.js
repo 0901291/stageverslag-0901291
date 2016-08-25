ecafa6c4ed05e2fdce00d9a3f0dc5b063294401e
Ian Wensink <ianwensink@gmail.com>
2016-08-25 13:36:06 +0200

    Routing: - Add pages/:id as route - Add redirect from /home to /

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, IndexRoute} from 'react-router';
+import {Route, IndexRoute, Redirect} from 'react-router';
 import App from './components/App';
 // import ManageAchievementPage from './components/achievement/ManageAchievementPage'; // Add/edit achievement
 // import AchievementPage from './components/achievement/AchievementPage'; // Achievement detail
@@ -8,13 +8,16 @@ import Page from './components/page/Page'; // Page detail
 
 export default (
     <Route path="/" component={App}>
+        <Redirect from="home" to="/" />
         <IndexRoute page="home" component={Page} />
         {/*<Route path="achievements/add" component={ManageAchievementPage} />*/}
         {/*<Route path="achievements/:id/edit" component={ManageAchievementPage} />*/}
         {/*<Route path="achievements/:id" component={AchievementPage} />*/}
         <Route path="pages/add" component={ManagePagePage} />
         <Route path="pages/:id/edit" component={ManagePagePage} />
-        <Route path=":page" component={Page} />
+        <Route path=":id" component={Page} />
+        <Route path="/pages/:id" component={Page} />
+        <Route path=":id/edit" component={ManagePagePage} />
         <Route path="notfound" page="notfound" component={Page} />
     </Route>
 );
